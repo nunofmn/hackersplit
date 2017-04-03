948ee56ce6d37f4aa9f9416cc9b190a33aa8707a
Nuno Nogueira <nunofmn@gmail.com>
2017-04-03 17:10:34 +0100

    View subcomments in interface.

--- src/components/CommentList/index.js
+++ src/components/CommentList/index.js
@@ -3,15 +3,15 @@ import React from 'react';
 import './style.css';
 import CommentItem from '../CommentItem/index';
 
-const getSubComments = (comment) => {
-  return ('kids' in comment) ? comment.kids : [];
+const isDeleted = (comment) => {
+  return 'deleted' in comment && comment.deleted;
 };
 
 const CommentList = ({comments}) => {
   return (
     <ul className="Comments">
       {comments.map((comment) => {
-        return <CommentItem key={comment.id} author={comment.by} content={comment.text} subComments={getSubComments(comment)} />;
+        return !isDeleted(comment) ? <CommentItem key={comment.id} comment={comment} /> : null;
       })}
     </ul>
   );
